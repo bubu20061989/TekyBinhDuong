3b8904bade6a13b867213a8b93f27a35d813c8c9
Hoang-3110 <huyhoangdaovu@gmail.com>
2024-09-12 19:55:01 +0700

    Hoàng

--- quanLyLopHoc.py
+++ quanLyLopHoc.py
@@ -1,53 +1,62 @@
 import tkinter as tk
 
-class ez3:
+class quanLyLopHoc:
     def __init__(self,master):
         self.master = master
-        self.master.title("ez")
+        self.master.title("Quản lý lớp học")
         self.master.geometry('400x400')
+        
+        self.widthEntry = 45
+        
+        self.x = 0
+        self.y = 0
+
+        self.Tx = 100
+
+        self.Tlbly = 50
 
         self.label_lop = tk.Label(self.master,text = 'Lớp:')
-        self.label_lop.place(x = 0, y= 0)
-        
-        self.entry_lop = tk.Entry(self.master)
-        self.entry_lop.place(x = 100, y = 0)
-        
+        self.label_lop.place(x = self.x, y = self.y)
+
+        self.entry_lop = tk.Entry(self.master,width = self.widthEntry)
+        self.entry_lop.place(x = self.x + self.Tx , y = self.y)
+  
         self.label_phong_hoc = tk.Label(self.master,text = 'Phòng học:')
-        self.label_phong_hoc.place(x = 0, y= 50)
-        
-        self.entry_phong_hoc = tk.Entry(self.master)
-        self.entry_phong_hoc.place(x = 100, y = 50)
-        
+        self.label_phong_hoc.place(x = self.x, y = self.Tlbly)
+
+        self.entry_phong_hoc = tk.Entry(self.master,width = self.widthEntry)
+        self.entry_phong_hoc.place(x = self.x + self.Tx, y = self.Tlbly)
+
         self.label_GVCN = tk.Label(self.master,text = 'GVCN')
-        self.label_GVCN.place(x = 0, y= 100)
+        self.label_GVCN.place(x = self.x, y = self.Tlbly * 2 )
         
-        self.entry_GVCN = tk.Entry(self.master)
-        self.entry_GVCN.place(x = 100, y = 100)
+        self.entry_GVCN = tk.Entry(self.master,width = self.widthEntry)
+        self.entry_GVCN.place(x = self.x + self.Tx, y = self.Tlbly * 2 )
         
         self.label_lop_truong = tk.Label(self.master,text = 'Lớp trưởng')
-        self.label_lop_truong.place(x = 0, y= 150)
+        self.label_lop_truong.place(x = self.x, y = self.Tlbly * 3 )
         
-        self.entry_lop_truong = tk.Entry(self.master)
-        self.entry_lop_truong.place(x = 100, y = 150)
+        self.entry_lop_truong = tk.Entry(self.master,width = self.widthEntry)
+        self.entry_lop_truong.place(x = self.x + self.Tx, y = self.Tlbly * 3 )
         
         self.label_lop_pho = tk.Label(self.master,text = 'Lớp phó')
-        self.label_lop_pho.place(x = 0, y= 200)
+        self.label_lop_pho.place(x = self.x, y = self.Tlbly * 4 )
         
-        self.entry_lop_pho = tk.Entry(self.master)
-        self.entry_lop_pho.place(x = 100, y = 200)
+        self.entry_lop_pho = tk.Entry(self.master,width = self.widthEntry)
+        self.entry_lop_pho.place(x = self.x + self.Tx, y = self.Tlbly * 4 )
         
         self.label_soluonghs = tk.Label(self.master,text = 'Số lượng học sinh')
-        self.label_soluonghs.place(x = 0, y = 250)
+        self.label_soluonghs.place(x = self.x, y = self.Tlbly * 5 )
         
-        self.entry_soluonghs = tk.Entry(self.master)
-        self.entry_soluonghs.place(x = 100, y = 250)
+        self.entry_soluonghs = tk.Entry(self.master,width = self.widthEntry)
+        self.entry_soluonghs.place(x = self.x + self.Tx, y = self.Tlbly * 5 )
         
-        self.login_button = tk.Button(self.master,text = 'Login',command = self.Login)
-        self.login_button.place(x = 150, y = 300 )
+        self.luuLopHoc_button = tk.Button(self.master,text = 'Lưu lớp học',command = self.luuLopHoc)
+        self.luuLopHoc_button.place(x = self.x + 175, y = self.Tlbly * 6 )
         
         
         
-    def Login(self):
+    def luuLopHoc(self):
         Lop = self.entry_lop.get()
         print(Lop)
         phong_hoc = self.entry_phong_hoc.get()
@@ -65,6 +74,6 @@ class ez3:
         
 if __name__ == "__main__":
     root = tk.Tk()
-    app = ez3(root)
+    app = quanLyLopHoc(root)
     root.mainloop()
 
